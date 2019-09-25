cce37b5e18585fb9068a9403cad6b85604119856
elducati <geomondi09@gmail.com>
2019-09-25 12:22:21 +0300

    modified sever.js response message

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('This is my server response');
+    res.end('This is my server response and i am ready to move on');
 });
 
 server.listen(process.env.PORT || 3000);
