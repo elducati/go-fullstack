17b84cff7d8ef9e56287d6a90602c1fcf68d2f42
elducati <geomondi09@gmail.com>
2019-09-26 16:50:01 +0300

    added put and delete middleware

--- backend/app.js
+++ backend/app.js
@@ -18,53 +18,119 @@ mongoose.connect('mongodb+srv://jeff007:tczJd3dArrDW3XGM@cluster0-shtxy.mongodb.
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 app.use(bodyParser.json());
 app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    thing.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Post saved successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
   });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'fddfdgv',
-            title: 'My first thing',
-            description: 'All of the info about my thing',
-            imageUrl: 'https://cdn.pixabay.com/photo/2017/03/27/14/48/tea-2179175_960_720.jpg',
-            price: 4900,
-            userId: 'dfdsfgfdsg'
-        },
-        {
-            _id: 'fddfdgv',
-            title: 'My second thing',
-            description: 'All of the info about my thing',
-            imageUrl: 'https://image.shutterstock.com/image-photo/cup-tea-mint-on-wooden-260nw-139065488.jpg',
-            price: 3700,
-            userId: 'dfdsfgfdsg'
-        },
-    ];
-    res.status(200).json(stuff);
+  const stuff = [
+    {
+      _id: 'fddfdgv',
+      title: 'My first thing',
+      description: 'All of the info about my thing',
+      imageUrl: 'https://cdn.pixabay.com/photo/2017/03/27/14/48/tea-2179175_960_720.jpg',
+      price: 4900,
+      userId: 'dfdsfgfdsg'
+    },
+    {
+      _id: 'fddfdgv',
+      title: 'My second thing',
+      description: 'All of the info about my thing',
+      imageUrl: 'https://image.shutterstock.com/image-photo/cup-tea-mint-on-wooden-260nw-139065488.jpg',
+      price: 3700,
+      userId: 'dfdsfgfdsg'
+    },
+  ];
+  res.status(200).json(stuff);
+});
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 module.exports = app;
